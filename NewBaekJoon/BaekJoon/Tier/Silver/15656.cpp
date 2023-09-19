83284430f446d099daa62eb1130a100cff66d9ee
DodoJBNU <dh723@jbnu.ac.kr>
2023-09-19 23:35:17 +0900

    백트래킹

---
+++ NewBaekJoon/BaekJoon/Tier/Silver/15656.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int N, M;
+vector<int>input;
+vector<int>res;
+
+void dfs(int depth)
+{
+	if (depth == M)
+	{
+		for (auto i : res)
+		{
+			cout << i << " ";
+		}
+		cout << "\n";
+	}
+	else
+	{
+		for (int i = 0; i < N; i++)
+		{
+			res[depth] = input[i];
+			dfs(depth + 1);
+		}
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	input.assign(N, 0);
+	res.assign(M, 0);
+
+	for (int i = 0; i < N; i++)
+		cin >> input[i];
+	
+	sort(input.begin(), input.end());
+	dfs(0);
+	return 0;
+}
